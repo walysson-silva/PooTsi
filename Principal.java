192927e8ecdc3b73d8e555cd0d6fe67c63b87e09
Wálysson Silva <walysson_silva@hotmail.com>
2022-05-12 12:45:59 -0300

    Criando classe Pessoa

---
+++ Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+  public static void main(String[] args) {
+    Pessoa p = new Pessoa("Maria", 12);
+
+    System.out.println(p.getNome());
+    System.out.println(p.getIdade());
+  }
+}
